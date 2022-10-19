ce9d90a8332d863888e9c4eab0baa009956511e0
ShamilHajiyev <shamilnh@code.edu.az>
2022-10-19 18:11:22 +0400

    initial commit

---
+++ Compare/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + " is greater");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num2 + " is greater");
+            }
+            else
+            {
+                Console.WriteLine("Entered numbers are equal");
+            }
+        }
+    }
+}
